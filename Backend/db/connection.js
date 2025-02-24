87a9f393dd23bfe1d97fe794ea82109b65c27dd5
JitRock22 <jitdas177883@gmail.com>
2025-02-24 23:42:27 +0530

    Database initialization phase-1

---
+++ Backend/db/connection.js
@@ -0,0 +1,5 @@
+const mongoose=require('mongoose');
+const url = "mongodb+srv://Chat_App:Jit%40123@cluster0.ek6ge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(url,{
+    
+})
