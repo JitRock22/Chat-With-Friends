2b95ae630310479d989f76ca97d8f8afe712d1cf
JitRock22 <jitdas177883@gmail.com>
2025-02-24 22:04:47 +0530

    Routes Creation and optimization

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,32 @@
 import './App.css'
 import Interface from './modules/Dashboard/Interface'
 import Form1 from './modules/Form/Form1'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 
+const ProtectedRoute = ({ children }) => {
+  const isLoggedIn = localStorage.getItem('user:token') !== null || true;
+  if (!isLoggedIn) {
+    return <Navigate to={"/user/signin"} />
+  } else if (isLoggedIn && ['/user/signin', 'user/signup'].includes(window.location.pathname)) {
+    return <Navigate to={'/'} />
+  }
+
+  return children
+}
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element:
+      <ProtectedRoute><Interface /></ProtectedRoute>
+  }, {
+    path: "/user/signin",
+    element: <ProtectedRoute><Form1 isSignin={true} /></ProtectedRoute>
+  },
+  {
+    path: "/user/signup",
+    element: <ProtectedRoute><Form1 isSignin={false} /></ProtectedRoute>
+  },
+]);
 function App() {
 
 
@@ -10,7 +35,8 @@ function App() {
     <>
       <div className=" h-screen flex justify-center items-center">
         {/* <Form1 /> */}
-        <Interface/>
+        {/* <Interface /> */}
+        <RouterProvider router={Router} />
       </div>
     </>
   )
