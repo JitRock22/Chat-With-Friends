44e974e130db01f254d689529ac40c60b45522d2
JitRock22 <jitdas177883@gmail.com>
2025-03-04 18:04:55 +0530

    hiding db info

--- Backend/db/connection.js
+++ Backend/db/connection.js
@@ -1,6 +1,7 @@
-const mongoose=require('mongoose');
-const url = "mongodb+srv://Chat_App:Jit%40123@cluster0.ek6ge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(url,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=> console.log("connected to DB")).catch((e)=>console.log('Error',e))
+require('dotenv').config();
+const mongoose = require('mongoose');
+const url = process.env.MONGODB_URI;
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("connected to DB")).catch((e) => console.log('Error', e))
